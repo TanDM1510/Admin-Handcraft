0c6e21d8b23cc5f54649726ecc30d0415d34dfb0
Duong Minh Tan (K16 HCM) <tandmss160653@fpt.edu.vn>
2024-06-24 10:10:33 +0700

    update

--- src/sections/users/UsersView.jsx
+++ src/sections/users/UsersView.jsx
@@ -1,11 +1,10 @@
-import ProductsFilter from "../products/ProductsFilter";
 import UsersHeader from "./UsersHeader";
 
 const UsersView = () => {
   return (
     <div>
       <UsersHeader />
-      <ProductsFilter />
+
       {/* <UsersTable /> */}
     </div>
   );
