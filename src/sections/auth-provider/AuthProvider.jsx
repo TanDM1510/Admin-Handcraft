2eba957c7d57f085e0af619df635d1d22c721b2a
Duong Minh Tan (K16 HCM) <tandmss160653@fpt.edu.vn>
2024-07-10 10:49:16 +0700

    update

--- src/sections/auth-provider/AuthProvider.jsx
+++ src/sections/auth-provider/AuthProvider.jsx
@@ -1,9 +1,25 @@
-import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
 const AuthProvider = ({ children }) => {
-  const token = localStorage.getItem("authToken");
-  if (!token) {
-    redirect("/auth");
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+
+    if (!token) {
+      router.push("/auth");
+    } else {
+      setLoading(false);
+    }
+  }, [router]);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
   return <>{children}</>;
 };
+
 export default AuthProvider;
